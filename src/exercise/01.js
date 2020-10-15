15ba0fce295f13d94fac3845c75b5967e45f9a53
alXmas <alexsandr.xmas@gmail.com>
2020-10-15 10:38:40 +0400

    resolve ex01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,20 +4,15 @@
 import React from 'react'
 
 function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
-
-  function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
-  }
-
+  const [name, setName] = React.useState('')
+  const handleChange = event => setName(event.target.value)
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
         <input onChange={handleChange} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {name ? <strong>Hello {name}</strong> : 'Enter your name'}
     </div>
   )
 }
