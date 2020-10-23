2eb46b0a35d64eab27008476123c347a05ae8e97
alXmas <alexsandr.xmas@gmail.com>
2020-10-23 09:58:11 +0400

    fix remark

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,22 +4,16 @@
 import React from 'react'
 
 function Greeting() {
-  // using useState
   const [name, setName] = React.useState(
-    // we are getting the name from localStorage or default to empty string
     window.localStorage.getItem('name') || ''
   );
 
-  // The Effect Hook lets you perform side effects in function components
-  // Load every time the Greeting() is rendered
   React.useEffect(() => {
-    // The read-only localStorage property allows you to access a Storage object for the
-    // document's origin; the stored data is saved across browser sessions.
     window.localStorage.setItem('name', name);
   });
 
   const handleChange = event => setName(event.target.value);
-  
+
   return (
     <div>
       <form>
